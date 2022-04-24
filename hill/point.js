f91e3f4fce54a1dcde931bbdea7d6453f79b51f4
woosungkim0123 <sibu2005@naver.com>
2022-04-24 21:59:06 +0900

    max random delete

--- hill/point.js
+++ hill/point.js
@@ -7,7 +7,8 @@ export class Point {
     this.speed = 0.1;
     this.cur = index;
     // 얼만큼 움직일 것인가
-    this.max = Math.random() * 100 + 150;
+    //this.max = Math.random() * 100 + 150;
+    this.max = 0.5 * 100 + 150;
   }
 
   update() {
